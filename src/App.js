1236f8836c0b402d3d4fbad9097e6aa4171c1009
Amit Bhandari <115782156+AmitBhhandari@users.noreply.github.com>
2022-11-20 13:34:38 +0530

    Simple React codes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ExpenseItem from "./Components/ExpenseItem";
+import SomeExpenses from "./Components/SomeExpenses";
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
+      <SomeExpenses></SomeExpenses>
+    </div>
+  );
+}
+
+export default App;
