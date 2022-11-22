f079094e441cf1e34f591720235d6b2f09678192
Amit Bhandari <115782156+AmitBhhandari@users.noreply.github.com>
2022-11-22 20:00:50 +0530

    sub folder and fat arrow

---
+++ src/Components/Expenses/ExpenseDetails.js
@@ -0,0 +1,13 @@
+const ExpenseDetail=(props)=>{
+    const title=props.title;
+    const amount=props.amount;
+
+    return(
+      
+            <div className="expense-item__description">
+              <h2>{title}</h2>
+              <div className="expense-item__price">{amount}</div>
+            </div>
+    )
+}
+export default ExpenseDetail;
