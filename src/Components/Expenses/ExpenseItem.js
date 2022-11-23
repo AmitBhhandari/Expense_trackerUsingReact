ae4df1d43ee689a3795c5279102f9c108b0af8ca
Amit Bhandari <115782156+AmitBhhandari@users.noreply.github.com>
2022-11-23 12:49:05 +0530

    eventListener added
    Delete expense and onClick  event created

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,21 +1,35 @@
-
 import ExpenseDetail from "./ExpenseDetails";
 import ExpenseDate from "./Expense date";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
+const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log("Clicked");
+  };
 
-const ExpenseItem=(props)=> {
-    
+  const parentNode = document.getElementsByClassName("expenses")[0];
+  const childNode = document.getElementsByClassName("expense-item")[0];
+  const DeleteHandler = () => {
+    console.log("delete");
+    // console.log(parentNode);
+    console.log(childNode);
+    if (childNode) {
+      parentNode.removeChild(childNode);
+      console.log("childnode is present");
+    }
+  };
 
   return (
     <Card className="expense-item">
-        <ExpenseDate date={props.date}/> 
-     
+      <ExpenseDate date={props.date} />
+
       <div className="expense-item__description">
-        <ExpenseDetail amount={props.amount} title={props.title}/>
+        <ExpenseDetail amount={props.amount} title={props.title} />
       </div>
+      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={DeleteHandler}>Delete Expense</button>
     </Card>
   );
-}
+};
 export default ExpenseItem;
