cd887d7401826428fb4d27bc937fa0f8e59fa309
Amit Bhandari <115782156+AmitBhhandari@users.noreply.github.com>
2022-11-23 20:26:17 +0530

    useState added
    useState added now React will Re-rendered the component when we hit button

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,34 +1,39 @@
+import React,{ useState } from 'react';
 import ExpenseDetail from "./ExpenseDetails";
 import ExpenseDate from "./Expense date";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  const clickHandler = () => {
-    console.log("Clicked");
-  };
+   const[title,setTitle]=useState(props.title);
+   const[amount,setAmount]=useState(props.amount);
 
-  const parentNode = document.getElementsByClassName("expenses")[0];
-  const childNode = document.getElementsByClassName("expense-item")[0];
-  const DeleteHandler = () => {
-    console.log("delete");
-    // console.log(parentNode);
-    console.log(childNode);
-    if (childNode) {
-      parentNode.removeChild(childNode);
-      console.log("childnode is present");
-    }
+ 
+  const clickHandler = () => {
+    setTitle('Updated');
+    console.log(title);
   };
+  
+  const AmountHandler=()=>{
+    setAmount(100)
+    console.log(amount)
+  }
+ 
+    
+  
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
 
       <div className="expense-item__description">
-        <ExpenseDetail amount={props.amount} title={props.title} />
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
-      <button onClick={DeleteHandler}>Delete Expense</button>
+      <button onClick={AmountHandler}>Change Amount</button>
+      
+      
     </Card>
   );
 };
