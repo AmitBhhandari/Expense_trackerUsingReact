ec6e468e787f966b30a7814acbdca25097cf3228
Amit Bhandari <115782156+AmitBhhandari@users.noreply.github.com>
2022-11-24 13:31:57 +0530

    Form Details
    showing user data on console and creating a form

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,20 +1,41 @@
+import React,{ useState } from 'react';
 import ExpenseDetail from "./ExpenseDetails";
 import ExpenseDate from "./Expense date";
 import "./ExpenseItem.css";
+import Card from "../UI/Card";
 
-function ExpenseItem(props) {
+
+const ExpenseItem = (props) => {
+   const[title,setTitle]=useState(props.title);
+   const[amount,setAmount]=useState(props.amount);
+
+ 
+  const clickHandler = () => {
+    setTitle('Updated');
+    console.log(title);
+  };
+  
+  const AmountHandler=()=>{
+    setAmount(100)
+    console.log(amount)
+  }
+ 
     
+  
 
   return (
-    <div className="expense-item">
-        <ExpenseDate date={props.date}/> 
-      <div>
-        <h3>{props.LocationOfExpenditure}</h3>
-      </div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+
       <div className="expense-item__description">
-        <ExpenseDetail amount={props.amount}/>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
-    </div>
+      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={AmountHandler}>Change Amount</button>
+      
+      
+    </Card>
   );
-}
+};
 export default ExpenseItem;
