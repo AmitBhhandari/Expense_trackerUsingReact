53428ecc60bf21849f9379514d3b9cc5e880bff3
Amit Bhandari <115782156+AmitBhhandari@users.noreply.github.com>
2022-11-24 13:32:28 +0530

    files added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import ExpenseItem from "./Components/ExpenseItem";
-import SomeExpenses from "./Components/SomeExpenses";
-function App() {
+import Expenses from "./Components/Expenses/Expenses";
+import NewExpenses from "./Components/NewExpenses/NewExpenses";
+
+const App=()=> {
+  const expenses = [
+    {
+      title: "car Insurence",
+      amount: 2000,
+      date: new Date(2022, 11, 20),
+      
+    },
+    {
+      title: "Fuel refill",
+      amount: 4000,
+      date: new Date(2022, 1, 29),
+     
+    },
+    {
+      title: "Rent",
+      amount: 20000,
+      date: new Date(2022, 12, 20),
+      
+    },
+    {
+      title: "Tour",
+      amount: 8000,
+      date: new Date(2022, 12, 21),
+      
+    },
+  ];
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
-      <SomeExpenses></SomeExpenses>
+      
+      <NewExpenses/>
+      <Expenses items={expenses}/>
+      
+  
     </div>
   );
 }
